08be8c7a27ff8d47bf76c07208f5bc7488799a38
Ryan Kwok <lifexperial@gmail.com>
2024-09-11 01:28:27 +0800

    added another print code (2nd one)

--- src/main/java/Lab1/mainApp1.java
+++ src/main/java/Lab1/mainApp1.java
@@ -8,6 +8,7 @@ public class mainApp1 {
         int b = 2;
         int n = 11;
         System.out.println("program starting");
+        System.out.println("program started");
         System.out.println(" one plus one is " + 2);
         System.out.println(b+" to power "+n+" = "+ myLibrary.Power(b,n));
         System.out.println(n+"! = " + myLibrary.factorial(n));
